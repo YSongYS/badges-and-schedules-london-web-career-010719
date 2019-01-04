ff0db6e8b0ee9445e3e8a58b92d8861deefb2177
Songyishu Yang <songyishu.yang@gmail.com>
2019-01-04 16:34:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,3 +10,9 @@ end
 
 def assign_rooms (attendees)
   attendees.each_with_index {|name,room| puts "Hellow, #{name}! You'll be assigned to room #{room}!"}
+end
+
+def printer (attendees)
+  batch_badge_creator(attendees)
+  assign_rooms(attendees)
+end
