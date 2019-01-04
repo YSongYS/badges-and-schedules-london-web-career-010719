b6830f5f08cfbd72866c85b66601c2e73a1c7215
Songyishu Yang <songyishu.yang@gmail.com>
2019-01-04 16:34:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,11 @@
 
 def badge_maker (name)
   puts "Hellow, my name is #{name}."
-end 
+end
 
 def batch_badge_creator (attendees)
-  attendees.each {|name| puts "Hellow, my name is #{name}."}
-end 
+  attendees.each {|name| puts "Hello, my name is #{name}."}
+end
+
+def assign_rooms (attendees)
+  attendees.each_with_index {|name,room| puts "Hellow, #{name}! You'll be assigned to room #{room}!"}
