02186015b3788415d7bdfa7a79b885803d23086f
Songyishu Yang <songyishu.yang@gmail.com>
2019-01-04 16:31:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
 # Write your code here.
+
+def badge_maker (name)
+  puts "Hellow, my name is #{name}."
+end 
+
+def batch_badge_creator (attendees)
+  attendees.each {|name| puts "Hellow, my name is #{name}."}
+end 
