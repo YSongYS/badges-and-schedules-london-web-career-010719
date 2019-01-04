6d9ce684d739284ed63754b45b3bcfa03265f6d0
Songyishu Yang <songyishu.yang@gmail.com>
2019-01-04 16:44:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ def badge_maker (name)
 end
 
 def batch_badge_creator (attendees)
-  return attendees.map {|name| "Hello, my name is" + name + "."}
+  return attendees.map {|name| "Hello, my name is " + name + "."}
 end
 
 def assign_rooms (attendees)
